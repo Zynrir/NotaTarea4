d0179b2a5d2aa98c0fad938d995c0fa64308a980
Jose Miguel <josemlg0312@gmail.com>
2023-05-30 00:00:41 +0200

    Utiliza las funciones de refactorización para cambiar la cabecera del método "cali". Le cambiarás el nombre por "califica" y, en el mismo paso, cambiarás el nombre del parámetro por "num_nota". El método anterior tendrá que quedar en desuso. (En Eclipse). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio.

--- nota/src/nota/nota.java
+++ nota/src/nota/nota.java
@@ -1,27 +1,54 @@
 package nota;
+
 import java.util.Scanner;
 
+/**
+ * La clase Notas es un programa que clasifica una calificación numérica
+ * ingresada por el usuario y la convierte en una cadena que representa su
+ * valoración, utilizando el método público cali.
+ */
 public class nota {
 
-public static void main(String[] args) {
-Scanner reader = new Scanner(System.in);// Para pedir un dato numérico por teclado
-int numero = 0;
-String nota = "";
+public static void main1(String[] args) {
 System.out.print("Introduzca el valor de una calificación: ");
-numero = reader.nextInt();
-
-if (numero >= 0 && numero < 5)
-nota = "Suspenso";
-else if (numero >= 5 && numero < 7)
-nota = "Aprobado";
-else if (numero >= 7 && numero <9)
-nota = "Notable";
-else if (numero >= 9 && numero <= 10)
-nota = "Sobresaliente";
-else
-nota = "El valor de la calificación introducida no es correcta";
-
-System.out.println(nota);
-reader.close();
 }
+
+    /**
+     * El método público cali clasifica una calificación numérica en función de
+     * su valor y devuelve una cadena que representa su valoración.
+     * @param numero la calificación numérica a clasificar
+     * @return la valoración correspondiente a la calificación
+     * @deprecated Use {@link #calificacion(int)} instead
+     */
+public static String cali(int numero) {
+	return calificacion(numero);
+}
+
+	/**
+     * El método público cali clasifica una calificación numérica en función de
+     * su valor y devuelve una cadena que representa su valoración.
+     * @param num_nota la calificación numérica a clasificar
+     * @return la valoración correspondiente a la calificación
+     */
+public static String calificacion(int num_nota) {
+  if (num_nota < 0 || num_nota > 10) {
+    return "El valor de la calificación introducida no es correcto";
+} else if (num_nota < 5) {
+    return "Suspenso";
+} else if (num_nota < 7) {
+    return "Aprobado";
+} else if (num_nota < 9) {
+    return "Notable";
+} else {
+    return "Sobresaliente";
+   	}
+}
+public static void main(String[] args) {
+   Scanner reader = new Scanner(System.in);
+   System.out.print("Introduzca el valor de una calificación: ");
+   	int n = reader.nextInt();
+    String c = calificacion(n);
+   System.out.println(c);
+    reader.close();
+    }
 }
