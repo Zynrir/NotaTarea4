59abcd62205f9d4e548928c640b38ce51f6d75f8
Jose Miguel <josemlg0312@gmail.com>
2023-05-29 23:40:40 +0200

    Utiliza las funciones de refactorización para sustituir el código que se encarga de especificar la calificación en función de la nota por un método que llamarás "cali", que será público, de forma que se incluyan los comentarios para la documentación si ello es posible. Muestra cómo se hace en los dos entornos. (En Eclipse). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercic

--- nota/src/nota/nota.java
+++ nota/src/nota/nota.java
@@ -4,24 +4,34 @@ import java.util.Scanner;
 public class nota {
 
 public static void main(String[] args) {
-Scanner reader = new Scanner(System.in);// Para pedir un dato numérico por teclado
-int n = 0;
-String c = "";
-System.out.print("Introduzca el valor de una calificación: ");
-n = reader.nextInt();
+calif();
+}
 
-if (n >= 0 && n < 5)
-c = "Suspenso";
-else if (n >= 5 && n < 7)
-c = "Aprobado";
-else if (n >= 7 && n <9)
-c = "Notable";
-else if (n >= 9 && n <= 10)
-c = "Sobresaliente";
-else
-c = "El valor de la calificación introducida no es correcta";
-
-System.out.println(c);
-reader.close();
+/**
+ * El método público cali clasifica una calificación numérica en función de
+ * su valor y devuelve una cadena que representa su valoración.
+ * @param n la calificación numérica a clasificar
+ * @return la valoración correspondiente a la calificación
+ */
+public static void calif() {
+	Scanner reader = new Scanner(System.in);// Para pedir un dato numérico por teclado
+	int numero = 0;
+	String nota = "";
+	System.out.print("Introduzca el valor de una calificación: ");
+	numero = reader.nextInt();
+	
+	if (numero >= 0 && numero < 5)
+	nota = "Suspenso";
+	else if (numero >= 5 && numero < 7)
+	nota = "Aprobado";
+	else if (numero >= 7 && numero <9)
+	nota = "Notable";
+	else if (numero >= 9 && numero <= 10)
+	nota = "Sobresaliente";
+	else
+	nota = "El valor de la calificación introducida no es correcta";
+	
+	System.out.println(nota);
+	reader.close();
 }
 }
